3370e8e4fb16ec1b45b2ee691632da677fbb6ad6
Gerrit <gerrit@hoekstra.co.uk>
2020-09-14 20:59:50 +0100

    Notes

--- experimental/DigitalInput/src/main.cpp
+++ experimental/DigitalInput/src/main.cpp
@@ -7,18 +7,16 @@
 #define DigitalInput_IO_2     39
 #define DigitalInput_IO_3     36
 
-// Dummy routine to explain what to do with your queue
 // Task to display running status from queue
 void ReadQueueTask(void* arg){
   DigitalInput * p = NULL;
   for(;;) {
-    if(xQueueReceive(DigitalInput::gpio_input_queue,(void *)&p, portMAX_DELAY)) {            
+    if(xQueueReceive(DigitalInput::gpio_input_queue,(void *)&p, portMAX_DELAY)) { 
+      // Process the changed inputs... or here, simply display them in the log.           
       ESP_LOGI(DigitalInput::TAG,"Sensor [%s] GPIO: [%u] State: [%d]", p->name.c_str(), p->gpio_num, p->now_state);
     }
   }
 }
-
-
 
 void app_main(void){  
   // Instantiate digital inputs  
@@ -27,11 +25,10 @@ void app_main(void){
   DigitalInput foot_2(static_cast<gpio_num_t>(DigitalInput_IO_2),"foot 2 down","first front right"); 
   DigitalInput foot_3(static_cast<gpio_num_t>(DigitalInput_IO_3),"foot 3 down","second front right");
 
-  // Get latest inputs off queue
+  // Create a task that gets the latest inputs off the queue, and in this case, only displays them in the log:
   xTaskCreate(ReadQueueTask, "ReadQueueTask", 2048, NULL, 10, NULL);
-  ESP_LOGW(DigitalInput::TAG, "Minimum free heap size: %d bytes", esp_get_minimum_free_heap_size());
 
-  // idle loop
+  // Idle loop: Constantly display the state of all the inputs:
   while(1){    
     ESP_LOGI(DigitalInput::TAG,"ISRCalls: [%llu] Sum of Highs: [%d] States: [%d%d%d%d]", DigitalInput::gpio_input_isr_counter, DigitalInput::gpio_input_high_count, foot_0.now_state, foot_1.now_state,foot_2.now_state,foot_3.now_state);    
     vTaskDelay(1000/portTICK_PERIOD_MS);
